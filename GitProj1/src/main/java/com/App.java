d021a8fee36bf826a511c101966a912b36b210cf
user <user@DESKTOP-RTFTBKU>
2024-02-27 16:56:23 +0530

    Gitproj1--created

---
+++ GitProj1/src/main/java/com/App.java
@@ -0,0 +1,15 @@
+package com;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public int  sum(int x,int y) 
+    {
+    	return x+y;
+    }
+}
