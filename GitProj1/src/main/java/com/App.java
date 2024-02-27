24666b3dfc45d5168f333068b38d20c0032a5018
user <user@DESKTOP-RTFTBKU>
2024-02-27 17:40:04 +0530

    jejejf

--- GitProj1/src/main/java/com/App.java
+++ GitProj1/src/main/java/com/App.java
@@ -3,5 +3,19 @@ package com;
 
 public class App 
 {
-	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+  
+    
+   
+    
+    System.out.println( "Hello World!" );
+    }
+    public int  sum(int x,int y) 
+    {
+    	return x+y;//klhslhslhse
+    }
+    
+    
 }
