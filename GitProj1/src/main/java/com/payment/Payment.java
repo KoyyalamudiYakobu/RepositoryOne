52a9419c82419ddfdeb4b889a828756d558d9a08
user <user@DESKTOP-RTFTBKU>
2024-02-28 20:17:07 +0530

    hhs

--- GitProj1/src/main/java/com/payment/Payment.java
+++ GitProj1/src/main/java/com/payment/Payment.java
@@ -3,14 +3,4 @@ package com.payment;
 public class Payment
 {
 
-	public void creditcardPaymnet() {
-		System.out.println("creditcardPaymnet() method colled");
-	}
-	
-	
-	
-	public void upiPaymnet()
-	{
-		System.out.println("upiPayment() method colled");
-	}
 }
