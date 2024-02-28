8259af11c6590a4b416a90fa29b8fa4b73c62afc
user <user@DESKTOP-RTFTBKU>
2024-02-28 20:20:00 +0530

    dev1-done by some task

--- GitProj1/src/main/java/com/payment/Payment.java
+++ GitProj1/src/main/java/com/payment/Payment.java
@@ -3,4 +3,16 @@ package com.payment;
 public class Payment
 {
 
+	public void creditcardPayment() {
+		System.out.println(" this is creditcardpayment() method");
+		
+	}
+	
+	public void upiPayment()
+	{
+		System.out.println(" this upiPayment() method colled");
+	}
+	
+	
+	
 }
