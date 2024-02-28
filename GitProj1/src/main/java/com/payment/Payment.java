f6772df7d410f9f6550f5c32e6ab061d41e2541f
user <user@DESKTOP-RTFTBKU>
2024-02-28 20:16:17 +0530

    dev1--done by some task credit and upi

--- GitProj1/src/main/java/com/payment/Payment.java
+++ GitProj1/src/main/java/com/payment/Payment.java
@@ -3,4 +3,14 @@ package com.payment;
 public class Payment
 {
 
+	public void creditcardPaymnet() {
+		System.out.println("creditcardPaymnet() method colled");
+	}
+	
+	
+	
+	public void upiPaymnet()
+	{
+		System.out.println("upiPayment() method colled");
+	}
 }
